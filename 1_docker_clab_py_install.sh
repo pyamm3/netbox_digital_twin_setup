98299c47f48ab65d916b5244afde12794f5ab5fc
pyamm3 <paragon.yamm3@gmail.com>
2025-07-01 15:22:06 -0400

    Update and rename 2_docker_install.sh to 1_docker_clab_py_install.sh

--- 1_docker_clab_py_install.sh
+++ 1_docker_clab_py_install.sh
@@ -19,12 +19,3 @@ bash -c "$(curl -sL https://get.containerlab.dev)"
 sudo apt install python3
 sudo apt install python3.10-venv
 
-echo "--- Creating Python3 virtual Env ---"
-python3 -m venv venv
-source ./venv/bin/activate
-#echo "--- Installing config management requirements ---"
-#pip install -r ./config_mgmt/requirements.txt
-echo "Run: 'source venv/bin/activate' to activate the venv."
-
-echo "--- Starting Virtual Enviorment ---"
-source venv/bin/activate
