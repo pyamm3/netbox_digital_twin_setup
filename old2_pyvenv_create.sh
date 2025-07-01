1d31cf5d4e93e2eabc8eca3d82ea9381210ea95c
pyamm3 <paragon.yamm3@gmail.com>
2025-07-01 15:22:31 -0400

    Create old2_pyvenv_create.sh

---
+++ old2_pyvenv_create.sh
@@ -0,0 +1,9 @@
+echo "--- Creating Python3 virtual Env ---"
+python3 -m venv venv
+source ./venv/bin/activate
+#echo "--- Installing config management requirements ---"
+#pip install -r ./config_mgmt/requirements.txt
+echo "Run: 'source venv/bin/activate' to activate the venv."
+
+echo "--- Starting Virtual Enviorment ---"
+source venv/bin/activate
